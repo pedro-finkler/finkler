4230678eb7fbe4f7c4009e8ffdfbf0b2d0d0e944
pedro-finkler <39865420+pedro-finkler@users.noreply.github.com>
2020-03-27 07:50:10 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <MainPage />
+  </div>
+</template>
+
+<script>
+import MainPage from "./components/MainPage.vue";
+
+export default {
+  name: "App",
+  components: {
+    MainPage
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  margin: 0 auto;
+  max-width: 40rem;
+  width: 100%;
+  padding: var(--rhythm);
+}
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <h1>Nice to meet you. I'm Pedro Finkler.</h1>
+    <p>
+      I'll keep this page update with my latest projects and experiences.
+    </p>
+    <h3>Projects</h3>
+    <ul>
+      <li>
+        <a
+          href="https://pedro-finkler.github.io/easy-border-radius/"
+          target="_blank"
+          rel="noopener"
+          >Easy Border Radius</a
+        >
+      </li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li>
+        <a
+          href="https://github.com/pedro-finkler"
+          target="_blank"
+          rel="noopener"
+          >GitHub</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 10px 10px;
+}
+a {
+  color: #427fb9;
+}
+</style>
